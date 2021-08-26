f43f6a11407f308bdcc295529a7f64a48a5434b9
Peterson Alves <pitersonet@hotmail.com>
2021-08-25 22:56:26 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,82 @@
+
+//Microondas
+/*Alimentos e seus respectivos tempos em segundos:
+1 - Pipoca – 10 segundos (padrão);
+2 - Macarrão – 8 segundos (padrão);
+3 - Carne – 15 segundos (padrão);
+4 - Feijão – 12 segundos (padrão);
+5 - Brigadeiro – 8 segundos (padrão);*/
+function microondas(opcao, tempo){
+    console.log("\nSelecione a opção em seguida o tempo:\n")
+    console.log("1 - Pipoca: 10 segundos")
+    console.log("2 - Macarrão: 8 segundos")
+    console.log("3 - Carne: 15 segundos")
+    console.log("4 - Feijão: 12 segundos")
+    console.log("5 - Brigadeiro: 8 segundos\n")
+    switch(opcao){
+        case 1: //Pipoca-10s
+            if(tempo > 20){
+                console.log("A comida queimou!!!")
+            }else if(tempo < 10){
+                console.log("Tempo insuficiente")
+            }else if(tempo > 30){
+                console.log("Kabuummmm!!!!!")
+            }else{
+                console.log("Prato pronto, bom apetite")
+            }
+            break;
+            case 2: //Macarrao-8s
+            if(tempo > 16){
+                console.log("A comida queimou!!!")
+            }else if(tempo < 8){
+                console.log("Tempo insuficiente")
+            }else if(tempo > 24){
+                console.log("Kabuummmm!!!!!")
+            }else{
+                console.log("Prato pronto, bom apetite")
+            }
+            break;
+        case 3: //Carne-15s
+            if(tempo > 30){
+                console.log("A comida queimou!!!")
+            }else if(tempo < 15){
+                console.log("Tempo insuficiente")
+            }else if(tempo > 45){
+                console.log("Kabuummmm!!!!!")
+            }else{
+                console.log("Prato pronto, bom apetite")
+            }
+            break;
+        case 4: //Feijao-12s
+            if(tempo > 24){
+                console.log("A comida queimou!!!")
+            }else if(tempo < 12){
+                console.log("Tempo insuficiente")
+            }else if(tempo > 36){
+                console.log("Kabuummmm!!!!!")
+            }else{
+                console.log("Prato pronto, bom apetite")
+            }
+            break;
+        case 5: //Brigadeiro-8s
+            if(tempo > 16){
+                console.log("A comida queimou!!!")
+            }else if(tempo < 8){
+                console.log("Tempo insuficiente")
+            }else if(tempo > 24){
+                console.log("Kabuummmm!!!!!")
+            }else{
+                console.log("Prato pronto, bom apetite")
+            }
+            break;
+            default: //Nenhuma das opções
+            console.log("Opção inválida!!!")
+            break;
+            }
+            }
+      
+        // teste pipoca 15s
+        microondas(2, 16)
+
+
+
